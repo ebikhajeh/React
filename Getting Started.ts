acc4592a96904adddc5885fbaaf5ea0a293effc7
ebikhajeh <ebikhajeh@gmail.com>
2025-03-07 15:38:53 -0800

    Update and rename part1.ts to Getting Started.ts

---
+++ Getting Started.ts
@@ -0,0 +1,16 @@
+we need : 
+          node -v
+          vscode + prettier extension (code/prefrences/settings => search for (format on save) )
+
+-> npm create vite@latest
+            Y
+      My-project
+        React
+      Type Script
+--------------------------------
+  cd My-project
+  npm install
+  npm run dev
+
+
+
