02d5c4dfacf3bb63fa1c2e1bcb2780d6357c5962
ebikhajeh <ebikhajeh@gmail.com>
2025-07-09 19:19:35 -0700

    Update service.cashierService.ts

--- service.cashierService.ts
+++ service.cashierService.ts
@@ -1,1 +1,7 @@
 
+import { cashier } from "../types/cashier";
+import HttpService from "./httpService";
+
+const cashierService = new HttpService<cashier>("/cashier");
+
+export default cashierService;
