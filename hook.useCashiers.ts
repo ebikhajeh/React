5f9cd8060a3a6e728757d0754fc9d863e8a77915
ebikhajeh <ebikhajeh@gmail.com>
2025-07-09 19:19:48 -0700

    Update hook.useCashiers.ts

--- hook.useCashiers.ts
+++ hook.useCashiers.ts
@@ -1,1 +1,13 @@
 
+// hooks/useCashiers.ts
+import { useQuery } from "@tanstack/react-query";
+import { Cashier } from "../types/cashier";
+import { ApiResponse } from "../types/apiResponse";
+import cashierService from "../services/cashierService";
+
+export const useCashiers = () => {
+  return useQuery<ApiResponse<Cashier[]>>({
+    queryKey: ["cashiers"],
+    queryFn: () => cashierService.getAll(),
+  });
+};
